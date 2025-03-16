776ae60746f74400c1228ba3906ee67a284801c1
Barento Muhedin <awesomemoi@hotmail.com>
2025-03-16 00:53:11 -0400

    just C works

--- ledbutton.py
+++ ledbutton.py
@@ -11,34 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-print("Pi running")
+
 import rpi_gpio as GPIO  # Import the QNX Raspberry Pi GPIO module for controlling GPIO pins
 import time # Import the time module for adding delays
-
-# board = [
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-# ]
 
 def buttonPressed(pin):
     # Check if the button (pin 20) is pressed
     if GPIO.input(16) == GPIO.LOW:
-        # Turn on the LED (pin 16)
+        # Turn on the LED (pin 16) cd ../qnxuser/homepage
         GPIO.output(26, GPIO.HIGH) 
     else:
         # Turn off the LED (pin 16)
         GPIO.output(26, GPIO.LOW) 
-
-    # Check if the button (pin 20) is pressed
-    if GPIO.input(20) == GPIO.LOW:
-        # Turn on the LED (pin 16)
-        GPIO.output(19, GPIO.HIGH) 
-    else:
-        # Turn off the LED (pin 16)
-        GPIO.output(19, GPIO.LOW) 
 
 # Set GPIO pin 16 as an output pin for controlling an LED
 GPIO.setup(26, GPIO.OUT)
@@ -49,16 +33,6 @@ GPIO.output(26, GPIO.LOW)
 GPIO.setup(16, GPIO.IN,GPIO.PUD_UP)
 # Add an event listener to detect button state changes on pin 20
 GPIO.add_event_detect(16, GPIO.BOTH, callback=buttonPressed)
-
-# Set GPIO pin 16 as an output pin for controlling an LED
-GPIO.setup(19, GPIO.OUT)
-# Ensure the LED is off initially
-GPIO.output(19, GPIO.LOW)
-
-# Set GPIO pin 20 as an input with an internal pull-up resistor
-GPIO.setup(20, GPIO.IN,GPIO.PUD_UP)
-# Add an event listener to detect button state changes on pin 20
-GPIO.add_event_detect(20, GPIO.BOTH, callback=buttonPressed)
 
 # Keep the script running to continuously monitor button presses
 while True:
